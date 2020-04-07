0738897f74946ed3c1a7177f76a892c7948c05f6
alejandrobarranco <alejandro.barranco@geoenlace.com>
2020-04-07 13:23:29 -0500

    Initial commit

---
+++ app/src/main/java/com/alexbar10/miwok/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alexbar10.miwok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
